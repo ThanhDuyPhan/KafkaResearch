0ab4e3b42a178a0040f3363c750f50157c03c196
T.Duy <thanhduyphan0512@gmail.com>
2024-03-15 14:38:01 +0700

    Make kafka producer & comsumer

--- KafkaResearch/Controllers/WeatherForecastController.cs
+++ KafkaResearch/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading;
 
 namespace KafkaResearch.Controllers
 {
@@ -6,6 +8,7 @@ namespace KafkaResearch.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private const string KAFKA_SERVER = "host1:9092";
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -28,6 +31,45 @@ namespace KafkaResearch.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost("msg")]
+        public async Task<OkResult> PushMessageAsync(string msg = "Hello Kafka")
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = KAFKA_SERVER,
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var result = await producer.ProduceAsync("weblog", new Message<Null, string> { Value = msg });
+            }
+            return Ok();
+        }
+
+        [HttpGet("msg")]
+        public async Task<OkResult> ListenMessageAsync()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = KAFKA_SERVER,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("weblog");
+
+                while (true)
+                {
+                    var consumeResult = consumer.Consume();
+                    Console.WriteLine(consumeResult.Value);
+                }
+
+                consumer.Close();
+            }
+            return Ok();
         }
     }
 }
